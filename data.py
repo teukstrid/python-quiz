b37fcf777b8cb7d17e305ed264b182f2931d3c4d
teukstrid <astridskillingstad@gmail.com>
2024-04-24 10:42:50 +0200

    Adding Quiz app

---
+++ data.py
@@ -0,0 +1,13 @@
+import requests
+
+
+parameters = {
+    "amount": 10,
+    "category": 18,
+    "type": "boolean",
+}
+
+response = requests.get(url="http://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]
